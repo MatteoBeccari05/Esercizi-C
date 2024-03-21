d46f3758a175bb0188987de464ad93400fefe125
Matteo Beccari <matteo.beccari@iisviolamarchesini.edu.it>
2024-03-21 16:32:08 +0100

    Eliminati

--- README.md
+++
@@ -1,9 +0,0 @@
-
-to compile:
-
-$ gcc exercise.c -o exercise
-
-
-to execute:
-
-$ ./exercise
--- Stringhe/fgets.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>  
-void main()   
-{   
-   char str[20];   
-   printf("Enter the string? ");  
-   fgets(str, 20, stdin);   
-   printf("%s", str);   
-}  
-//assicurarsi che non vengano letti più caratteri
