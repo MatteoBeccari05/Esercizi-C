3efcd20e84d77671d7c8d314aeb8121cfd133ada
Matteo Beccari <matteo.beccari@iisviolamarchesini.edu.it>
2024-03-21 16:29:58 +0100

    Create es1.c

---
+++ 3/Puntatori/es1.c
@@ -0,0 +1,22 @@
+/*Dati due numeri si scriva in linguaggio C una funzione di tipo void denominata scambia,
+che dati in input 2 numeri li scambi.*/
+
+#include <stdio.h>
+
+void scambia(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int a = 10, b = 15;
+
+	scambia(&a, &b);
+
+	printf("dopo lo scambio a vale: %d\n", a);
+	printf("dopo lo scambio b vale: %d\n", b);
+}
