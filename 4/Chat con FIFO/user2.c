0286a2fbce0d45ba6d1ac33f6dfe80b2ad5a948f
Matteo Beccari <matteo.beccari@iisviolamarchesini.edu.it>
2024-03-21 16:31:20 +0100

    Create user2.c

---
+++ 4/Chat con FIFO/user2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define SIZE 1024
+
+int main(int argc, char *argv[])
+{
+    int fd1 = open("fifo1", O_RDONLY);
+    int fd2 = open("fifo2", O_WRONLY);
+    char lettura[SIZE];
+    char scrittura[SIZE];
+
+    printf("Digitare HALT per terminare la chat\n");
+
+    do
+    {
+        read(fd1, lettura, sizeof(lettura));
+        printf("Utente 1: %s\n", lettura);
+        printf("Utente 2: ");
+        //scanf("%s", scrittura);
+        fgets(scrittura, sizeof(scrittura), stdin);
+        write(fd2, scrittura, sizeof(scrittura));
+
+    } while (strcmp(scrittura, "HALT"));
+    
+    close(fd1);
+    close(fd2);
+    return 0;
+}
