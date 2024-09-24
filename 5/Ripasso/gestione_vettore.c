d1d55c3077ecb3609110eb9958d890e15834b203
matteo <matteo.beccari@iisviolamarchesini.edu.it>
2024-09-24 14:50:57 +0200

    esercizio completato

--- 5/Ripasso/gestione_vettore.c
+++ 5/Ripasso/gestione_vettore.c
@@ -10,9 +10,127 @@ penultimo e cosi via;
 5) Visualizzi a video tutti i numeri dispari;
 6) Ricerchi un numero (letto in input) nell’array;
 7) Elimini un elemento (letto in input) dell’array (se esistente);
-8) Alterni (o scambi ) a due a due le posizioni del vettore: esempio
+8) Alterni (o scambi) a due a due le posizioni del vettore: esempio
 vettore: 1,2,3,4 vettore alternato: 2,1,4,3 (attenzione se lungo dispari);
 9) Ordini il vettore (ordinamento a scelta).*/
+
+void Stampa(int n1, int vettore[])
+{
+    printf("Vettore:");
+    printf("\n");
+    for(int i=0; i<n1 ;i++)
+    {
+        printf("Posizione %d: %d \n", i,vettore[i]);
+    }
+}
+
+void StampaMediaSomma(int a, int vettore[])
+{
+    float somma; 
+    float media;
+    for(int i=0; i<a; i++)
+    {
+        somma = somma + vettore[i];
+    }
+
+    media = (somma / a);
+    printf("La somma dei numeri è: %f", somma);
+    printf("\nLa media dei numeri è: %f", media);
+
+}
+
+void StampaNumeriPari(int a, int vettore[])
+{
+    printf("I numeri pari sono: ");
+    printf("\n");
+    for(int i=0; i<a; i++)
+    {
+        if(vettore[i]%2 == 0)
+        {
+            printf("%d \n",vettore[i]);
+        }
+    }  
+}
+
+void StampaNumeriDispari(int a, int vettore[])
+{
+    printf("I numeri dispari sono: ");
+    printf("\n");
+    for(int i=0; i<a; i++)
+    {
+        if(vettore[i]%2 == 1)
+        {
+            printf("%d \n",vettore[i]);
+        }
+    }  
+}
+
+void RicercaNumero(int a, int vettore[])
+{
+    int numero;
+    printf("Inserisci il numero da cercare: ");
+    scanf("%d",&numero);
+    for(int i=0; i<a ;i++)
+    {
+        if(vettore[i] == numero)
+        {
+            printf("Numero %d trovato in posizione %d\n", numero, i);
+        }
+    }
+}
+
+void EliminaNumero(int a, int vettore[])
+{
+    int t,k=0,cont=0;
+    int pos,vettore2[100];
+    printf("Inserisci il numero da eliminare: ");
+    scanf("%d",&t);
+    for(int i=0; i<a; i++)
+    {
+        if(vettore[i] != t)
+        {
+          vettore2[k] = vettore[i];
+          k = k + 1;              
+        }
+    }   
+
+    for(int i=0; i<k; i++)
+    {
+        vettore[i] = 0;
+        vettore[i] = vettore2[i];
+    }
+    vettore[a-1] = 0;
+}
+
+void ALternaNumeri(int a, int vettore[])
+{ 
+    int app;   
+    for (int i=0; i<a-1; i=i+2)
+    {
+        app = vettore[i];
+        vettore[i] = vettore[i+1];
+        vettore[i+1] = app;
+    }
+    Stampa(a, vettore);
+}
+
+void Ordina(int a, int vettore[])  //dal più grande al più piccolo
+{ 
+    int app;   
+    for (int k=0; k<a-1; k++)
+    {
+        for (int i=k+1; i<a; i++)
+        {
+            if (vettore[k] < vettore[i])
+            {
+               app = vettore[k];
+               vettore[k] = vettore[i];
+               vettore[i] = app;
+            }       
+        }
+    } 
+    Stampa(a, vettore);
+} 
 
 void StampaMenu()
 { 
@@ -28,14 +146,14 @@ void StampaMenu()
     printf("9) Ordina il vettore\n");
     printf("10) Esci dal programma\n");
     printf("-----------------------------------\n");
-    printf("Inserisci una scelta:\n");
+    printf("Inserisci una scelta:");
 }
 
 void Lettura(int *n1, int vettore[])
 { 
     do
     {
-        printf("Quanti numeri vuoi inserire: \n");
+        printf("Quanti numeri vuoi inserire: ");
         scanf("%d",n1);
 
     }while(n1 <= 0);
@@ -47,30 +165,11 @@ void Lettura(int *n1, int vettore[])
     }
 }
 
-void Stampa(int n1, int vettore[])
-{
-    printf("Vettore:");
-    printf("\n");
-    for(int i=0; i<n1 ;i++)
-    {
-        printf("Posizione %d: %d \n", i,vettore[i]);
-    }
-}
-
 void InvertiVettore(int a, int vettore[])
 {
-    /*int i,k,temp;
-    k=n1;
-    for (i = 0; i < (n1/2); i++) 
-    {
-        temp=vett1[i];
-        vett1[i]=vett1[k-1];
-        vett1[k-1]=temp;
-        k--;
-    }
-    Stampa(n1,vettore);*/
-
-    for(int i = a; i<=0; i--)
+    printf("Vettore invertito:");
+    printf("\n");
+    for(int i = a; i>=0; i--)
     {
         printf("Posizione %d: %d \n", i,vettore[i]);
     }
@@ -85,7 +184,7 @@ int main()
     do
     {
         StampaMenu();
-        scanf("%d", &b); //legge la scelta dal menù
+        scanf("%d", &b);
         switch (b)
 	    {
             case 1: 
@@ -94,27 +193,27 @@ int main()
 		    case 2: 
 	        InvertiVettore(a, vettore);
 		    break;
-		    /*case 3: 
-	        dispari(a, vett);
+		    case 3: 
+	        StampaMediaSomma(a, vettore);
 		    break;
 		    case 4: 
-		    inverti (a, vett);
+		    StampaNumeriPari (a, vettore);
 		    break;
 		    case 5: 
-		    cerca(a, vett);
+		    StampaNumeriDispari(a, vettore);
 		    break;
 		    case 6: 
-		    ordina(a, vett);
+		    RicercaNumero(a, vettore);
 		    break;
 		    case 7: 
-		    elimina1(a, vett);
+		    EliminaNumero(a, vettore);
 		    break;
 		    case 8: 
-		    elimina2(a, vett);
+		    ALternaNumeri(a, vettore);
 		    break;
 		    case 9: 
-		    alterna(a, vett);
-		    break;*/
+		    Ordina(a, vettore);
+		    break;
 	    }
 
     }while(b<10);
