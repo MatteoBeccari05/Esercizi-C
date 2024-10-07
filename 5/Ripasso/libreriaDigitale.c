4446ecacb8f9478566b447bd8fbe9d289fc36d9e
Matteo Beccari <98811589+MatteoBeccari05@users.noreply.github.com>
2024-10-07 16:22:28 +0200

    esercizio sistemato
    aggiunta ricerca per titolo

--- 5/Ripasso/libreriaDigitale.c
+++ 5/Ripasso/libreriaDigitale.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STRINGA 100
-#define LIBRI 100
+#define DIM 100
+#define NUMERO_LIBRI 100
 #define BUFFER_DIM 1024
 #define CATEGORIE 10
 
 /// @brief Struttura di un libro
 typedef struct Libro 
 {
-    char titolo[MAX_STRINGA];
-    char autore[MAX_STRINGA];
+    char titolo[DIM];
+    char autore[DIM];
     int anno;
     float prezzo;
-    char categoria[MAX_STRINGA];
+    char categoria[DIM];
 } Libro;
 
 /// @brief Struttura di una categoria di libri
 typedef struct Categoria 
 {
-    char nome[MAX_STRINGA];
-    Libro libri[LIBRI];
+    char nome[DIM];
+    Libro libri[NUMERO_LIBRI];
     int numLibri;
 } Categoria;
 
@@ -31,26 +31,24 @@ typedef struct Categoria
 /// @param categorie Array di categorie
 /// @param numCategorie Puntatore al numero di categorie presenti nel catalogo
 /// @return Numero di libri letti
-int leggiLibriDaFile(const char *filename, Libro libri[], Categoria categorie[], int *numCategorie) 
+int Lettura_Libri(const char *nome_file, Libro libri[], Categoria categorie[], int *numCategorie) 
 {
     char buffer[BUFFER_DIM];
-    FILE *file = fopen(filename, "r");
-    if (!file) //controllo che il file si apra correttamente
+    int numLibri = 0;
+    FILE *file = fopen(nome_file, "r");
+    if (!file) 
     {
-        perror("Errore nell'apertura del file");
-        return -1; // Restituisci -1 in caso di errore
+        printf("Errore nell'apertura del file");
+        exit(0);
     }
 
-    // Ignoro la prima riga del file csv (dove è presente l'intestazione)
+    // Ignora l'intestazione
     fgets(buffer, sizeof(buffer), file);
-    
-    int numLibri = 0;
 
-    while (fgets(buffer, sizeof(buffer), file)) //leggo finchè non finisce il file
+    while (fgets(buffer, sizeof(buffer), file)) 
     {
         Libro libro;
-        //leggo una riga dal file csv inserendo i vari paramentri nella variabile libro
-        sscanf(buffer, "%99[^,],%99[^,],%d,%f,%99[^\n]", libro.titolo, libro.autore, &libro.anno, &libro.prezzo, libro.categoria); 
+        sscanf(buffer, "%99[^,],%99[^,],%d,%f,%99[^\n]", libro.titolo, libro.autore, &libro.anno, &libro.prezzo, libro.categoria);
 
         // Aggiungi il libro all'array di libri
         libri[numLibri++] = libro;
@@ -62,7 +60,6 @@ int leggiLibriDaFile(const char *filename, Libro libri[], Categoria categorie[],
             if (strcmp(categorie[i].nome, libro.categoria) == 0) 
             {
                 categorie[i].libri[categorie[i].numLibri++] = libro;
-                //se la categoria viene trovata imposto trovato a 1
                 trovato = 1;
                 break;
             }
@@ -71,14 +68,12 @@ int leggiLibriDaFile(const char *filename, Libro libri[], Categoria categorie[],
         // Se la categoria non esiste, la creo
         if (!trovato && *numCategorie < CATEGORIE) 
         {
-            //se la categoria non esiste la creo
             strcpy(categorie[*numCategorie].nome, libro.categoria);
             categorie[*numCategorie].libri[0] = libro;
             categorie[*numCategorie].numLibri = 1;
             (*numCategorie)++;
         }
     }
-    //chiudo il file csv quando finisco di leggere tutti i dati
     fclose(file);
     return numLibri; // Restituisci il numero di libri letti
 }
@@ -86,62 +81,58 @@ int leggiLibriDaFile(const char *filename, Libro libri[], Categoria categorie[],
 /// @brief Visualizza a schermo tutti i libri
 /// @param libri Array di libri
 /// @param n Numero di libri
-void VisualizzaTuttiLibri(Libro libri[], int n) 
+void Visualizzazione_Catalogo_Completo(Libro libri[], int numeroLibri) 
 {
-    printf("|----------------------------------------------------|----------------------|------|--------|-----------------------|\n");
-    printf("| Titolo                                             | Autore               | Anno | Prezzo | Categoria             |\n");
-    printf("|----------------------------------------------------|----------------------|------|--------|-----------------------|\n");
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
+    printf("| Titolo                                                       | Autore                         | Anno | Prezzo | Categoria             |\n");
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
 
-    //creo una visualizzazione tabellare
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < numeroLibri; i++) 
     {
-                //spaziature
-        printf("| %-50s | %-20s | %-4d | %-6.2f | %-20s |\n", libri[i].titolo, libri[i].autore, libri[i].anno, libri[i].prezzo, libri[i].categoria);
+        printf("| %-60s | %-30s | %-4d | %-6.2f | %-20s |\n", libri[i].titolo, libri[i].autore, libri[i].anno, libri[i].prezzo, libri[i].categoria);
     }
 
-    printf("|----------------------------------------------------|----------------------|------|--------|-----------------------|\n");
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
 }
 
 /// @brief Stampa il menù di scelta
 /// @return Ritorna la scelta dell'utente
-int Menu() 
+int StampaMenu() 
 {
     int scelta;
-    printf("\n============================================================");
+    printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
     printf("\n1)  Visualizza tutti i libri");
     printf("\n2)  Visualizza libri per categoria");
-    printf("\n3)  Esci dal programma");
-    printf("\n============================================================\n");
+    printf("\n3)  Cerca il titolo di un libro");
+    printf("\n4)  Esci dal programma");
+    printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf("Scelta: ");
     scanf("%d", &scelta);
-    return scelta; //ritorno la scelta dell'utente
+    return scelta;
 }
 
 /// @brief Stampa i libri di una determinata categoria
 /// @param categoria Categoria da stampare
-void VisualizzaLibriPerCategoria(Categoria categoria) 
+void Visualizzazione_Per_Categoria(Categoria categoria) 
 {
     printf("\nLibri nella categoria '%s':\n", categoria.nome);
-    printf("|----------------------------------------------------|----------------------|------|--------|\n");
-    printf("| Titolo                                             | Autore               | Anno | Prezzo |\n");
-    printf("|----------------------------------------------------|----------------------|------|--------|\n");
-    
-    //creo una visualizzazione di tipo tabellare
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|\n");
+    printf("| Titolo                                                       | Autore                         | Anno | Prezzo |\n");
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|\n");
+
     for (int i = 0; i < categoria.numLibri; i++) 
     {
-                //spaziature 
-        printf("| %-50s | %-20s | %-4d | %-6.2f |\n", categoria.libri[i].titolo, categoria.libri[i].autore, categoria.libri[i].anno, categoria.libri[i].prezzo);
+        printf("| %-60s | %-30s | %-4d | %-6.2f |\n", categoria.libri[i].titolo, categoria.libri[i].autore, categoria.libri[i].anno, categoria.libri[i].prezzo);
     }
 
-    printf("|----------------------------------------------------|----------------------|------|--------|\n");
+    printf("|--------------------------------------------------------------|--------------------------------|------|--------|\n");
 }
 
 /// @brief Visualizza le varie categorie presenti nel catalogo
 /// @param categorie Struttura di categorie
 /// @param numCategorie Numero delle categorie
-void VisualizzaCategorie(Categoria categorie[], int numCategorie) 
+void VisualizzazioneCategorie(Categoria categorie[], int numCategorie) 
 {
-    //visualizzo le categorie in modo che l'utente riesca a scieglierne una per visualizzare i libri di una determinata categoria
     printf("\nCategorie disponibili:\n");
     for (int i = 0; i < numCategorie; i++) 
     {
@@ -149,14 +140,46 @@ void VisualizzaCategorie(Categoria categorie[], int numCategorie)
     }
 }
 
-int main() 
+/// @brief Visualizza le caratteristiche di un libro dato il titolo
+/// @param libri Array di libri
+/// @param numLibri Numero di libri
+void VisualizzazionePerTitolo(Libro libri[], int numLibri) 
+{
+    char titolo_da_cercare[DIM];
+    printf("Inserisci il titolo del libro: ");
+    getchar(); // Per consumare il newline rimasto
+    fgets(titolo_da_cercare, sizeof(titolo_da_cercare), stdin);
+    titolo_da_cercare[strcspn(titolo_da_cercare, "\n")] = 0; // Rimuovere il newline
+
+    int trovato = 0;
+    for (int i = 0; i < numLibri; i++) 
+    {
+        if (strcasecmp(libri[i].titolo, titolo_da_cercare) == 0) // Ignora maiuscole e minuscole
+        {
+            printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
+            printf("| Titolo                                                       | Autore                         | Anno | Prezzo | Categoria             |\n");
+            printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
+            printf("| %-60s | %-30s | %-4d | %-6.2f | %-20s |\n", libri[i].titolo, libri[i].autore, libri[i].anno, libri[i].prezzo, libri[i].categoria);
+            printf("|--------------------------------------------------------------|--------------------------------|------|--------|-----------------------|\n");
+            trovato = 1;
+            break;
+        }
+    }
+
+    if (!trovato) //se non trovo il libro desiderato
+    {
+        printf("Nessun libro trovato con il titolo '%s'\n", titolo_da_cercare);
+    }
+}
+
+int main(int argc, char *argv[]) 
 {
     Categoria categorie[CATEGORIE] = {0};
-    Libro libri[LIBRI];
-    const char *filename = "libri.csv"; // Nome del file CSV
+    Libro libri[NUMERO_LIBRI];
+    const char *nome_file = argv[1]; // Nome del file CSV
     int scelta;
     int numeroCategorie = 0;
-    int numLibri = leggiLibriDaFile(filename, libri, categorie, &numeroCategorie);  //leggo i libri dal file csv
+    int numLibri = Lettura_Libri(nome_file, libri, categorie, &numeroCategorie);
 
     if (numLibri < 0) 
     {
@@ -165,45 +188,50 @@ int main()
 
     do 
     {
-        scelta = Menu();
+        scelta = StampaMenu();
         switch (scelta) 
         {
             case 1:  //visualizzo tutti i libri
             {
-                VisualizzaTuttiLibri(libri, numLibri);
+                Visualizzazione_Catalogo_Completo(libri, numLibri);
                 break;
             }
             
             case 2:    //visualizza tutti i libri di una determinata categoria
             {
-                VisualizzaCategorie(categorie, numeroCategorie);
+                VisualizzazioneCategorie(categorie, numeroCategorie);
                 printf("Inserisci il numero della categoria: ");
                 int n;
-                //leggo il numero di categoria scelto dall'utente
                 scanf("%d", &n);
                 if (n > 0 && n <= numeroCategorie) 
                 {
-                    VisualizzaLibriPerCategoria(categorie[n - 1]);
-                } else 
+                    Visualizzazione_Per_Categoria(categorie[n - 1]);
+                } 
+                else 
                 {
                     printf("Categoria non valida.\n");
                 }
                 break;
             }
+
+            case 3:   //visualizza un libro con titolo scelto dall'utente
+            {
+                VisualizzazionePerTitolo(libri, numLibri);
+                break;
+            }
                 
-            case 3:
+            case 4:
             {
                 printf("Uscita dal programma.\n");
                 break;
             }
                 
             default:
-            {
                 printf("Scelta non valida.\n");
-            }    
         }
 
-    } while (scelta < 3);
+    } while (scelta < 4);
 
     return 0;
+
 }
