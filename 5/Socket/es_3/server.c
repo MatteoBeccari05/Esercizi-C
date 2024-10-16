6a3f9569566add3a41f893c5816ad6c4900cd398
Matteo Beccari <matteo.beccari@iisviolamarchesini.edu.it>
2024-10-16 10:26:15 +0200

    esercizio 3 stringa maiuscola server

---
+++ 5/Socket/es_3/server.c
@@ -0,0 +1,21 @@
+//SERVER
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+
+#define DIM 512
+#define SERVERPORT 1313
+
+int main()
+{
+    
+}
