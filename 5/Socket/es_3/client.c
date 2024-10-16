e92c17f25df0f11607be99bc02b2afe89c8ada36
Matteo Beccari <matteo.beccari@iisviolamarchesini.edu.it>
2024-10-16 10:25:46 +0200

    esercizio 3 stringa maiuscola

---
+++ 5/Socket/es_3/client.c
@@ -0,0 +1,18 @@
+//CLIENT
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+
+#define DIM 512
+#define SERVERPORT 1313
+
+int main()
+{
+
+}
